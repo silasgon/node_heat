7bc4c5e705ee5763737869b1f9edc8db78fdf12b
Silas Gonm <silasidc@gmail.com>
2021-10-19 18:49:42 -0300

    config de socket io

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import "dotenv/config";
 import express, { request, response } from "express";
+import http from "http";
+import { Server } from 'socket.io';
+import cors from 'cors';
 
 import { router } from "./routes"
 
 const app = express();
+app.use(cors());
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*",
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`)
+});
 
 app.use(express.json());
 
